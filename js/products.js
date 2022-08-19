5cd3f67d7ecc492e93c0e9aa2607c25313d6895e
v3ro7 <veronicapiriz33@gmail.com>
2022-08-19 20:50:25 -0300

    funcionalidad productos agregada

---
+++ js/products.js
@@ -0,0 +1,41 @@
+const LIST_URL = "https://japceibal.github.io/emercado-api/cats_products/101.json";
+
+let categoriesArray = [];
+
+function showCategoriesList(array){
+    let htmlContentToAppend = "";
+
+    for(let i = 0; i < array.length; i++){ 
+        let category = array[i];
+        htmlContentToAppend += `
+        <div class="list-group-item list-group-item-action">
+            <div class="row">
+                <div class="col-3">
+                    <img src="` + category.image + `" alt="product image" class="img-thumbnail">
+                </div>
+                <div class="col">
+                    <div class="d-flex w-100 justify-content-between">
+                        <div class="mb-1">
+                        <h4>`+ category.name + " " + "-" + " " + category.currency + " " + category.cost +`</h4> 
+                        <p> `+ category.description +`</p> 
+                        </div>
+                        <small class="text-muted">` + category.soldCount + ` vendidos</small> 
+                    </div>
+
+                </div>
+            </div>
+        </div>
+        `
+        document.getElementById("cat-list-container").innerHTML = htmlContentToAppend; 
+    }
+};
+
+document.addEventListener("DOMContentLoaded", function(e){
+    getJSONData(LIST_URL).then(function(resultObj){
+        if (resultObj.status === "ok")
+        {
+            categoriesArray = resultObj.data;
+            showCategoriesList(categoriesArray.products);
+        }
+    });
+});
