a67ce63aee5e916645bc57cedbd9974a5e9817b9
v3ro7 <veronicapiriz33@gmail.com>
2022-09-30 22:36:06 -0300

    Entrega 4
    1. Productos relacionados
    2. Menú desplegable de usuario
    3. Carrusel de imágenes

--- js/product-info.js
+++ js/product-info.js
@@ -1,25 +1,64 @@
 let linkUsuario = localStorage.getItem('User');
 document.getElementById('user').innerHTML = linkUsuario;
 let divProductInfo = document.getElementById('product-info');
+let divRelatedProducts = document.getElementById('related-products');
 let divProductComment = document.getElementById('product-comment');
 let divNewComment = document.getElementById('newComment');
 let btnComment = document.getElementById('btnComment');
+
+// re utilizo función para guardar un id de producto
+function setProductID(id){
+    localStorage.setItem("productID", id);
+    window.location = "product-info.html";
+}
+
 
 let idProducto = localStorage.getItem('productID');
 const PRODUCT_URL = `https://japceibal.github.io/emercado-api/products/${idProducto}.json`;
 const COMMENT_URL = `https://japceibal.github.io/emercado-api/products_comments/${idProducto}.json`;
 
+
+
 //función para iterar por las imagenes y mostrarlas
-function showImages(arrImg) {
-    let imagesToAppend = "";
-    for (let i=0; i<arrImg.length; i++) {
-        imagesToAppend += `
-            <img class="border p-1" src="${arrImg[i]}">
-        `;
+// function showImages(arrImg) {
+//     let imagesToAppend = "";
+//     for (let i=0; i<arrImg.length; i++) {
+//         imagesToAppend += `
+//         <div class="carousel-item mt-2">
+//             <img class="d-block w-50 border p-1" src="${arrImg[i]}">
+//         </div>
+//         `;
+//     }
+//     document.getElementById('images').innerHTML = imagesToAppend;
+// }
+
+
+// función para agregar clase active al primer div del carrusel
+function setActiveClass() {
+    let arrOfItems = document.getElementsByClassName('carousel-item');
+    let array = [];
+    for (let i=0; i<arrOfItems.length; i++) {
+        array.push(arrOfItems[i])
     }
-    document.getElementById('images').innerHTML = imagesToAppend;
+    console.log(array)
 }
 
+// función para iterar por el array de productos relacionados y mostrarlos
+function showRelated(arr) {
+    let content = "";
+    for (let i=0; i<arr.length; i++) {
+        let name = arr[i].name;
+        let image = arr[i].image;
+        let id = arr[i].id;
+        content += `
+            <div onclick="setProductID(${id})" class="border p-1 cursor-active">
+                <img src="${image}">
+                <p class="fs-5 text-center text-muted mt-3">${name}</p>
+            </div>
+        `;
+    }
+    document.getElementById('related-images').innerHTML = content;
+}
 
 
 //función para iterar por los comentarios y mostrarlos
@@ -47,6 +86,7 @@ function showComments(arrComments) {
     divProductComment.innerHTML = commentsToAppend;
 }
 
+let relatedProductsArr = [];
 // trae y muestra info del producto una vez que la página haya cargado
 document.addEventListener('DOMContentLoaded', function() {
     let images = [];
@@ -54,6 +94,9 @@ document.addEventListener('DOMContentLoaded', function() {
     .then(response => response.json())
     .then(data => {
         images = data.images;
+        console.log(images.length)
+        relatedProductsArr = data.relatedProducts;
+
         divProductInfo.innerHTML = `
         <h2 class="mt-4 mb-5">${data.name}</h2>
         <hr>
@@ -70,11 +113,44 @@ document.addEventListener('DOMContentLoaded', function() {
         <p>${data.soldCount}</p>
         <br>
         <p><strong>Imágenes ilustrativas</strong></p>
-        
-        <div class="mt-2" id="images">
+
+        <div id="carouselControls" class="carousel slide w-50" data-bs-ride="carousel">
+            <div class="carousel-inner" id="images">
+                <div class="carousel-item mt-2 active">
+                    <img class="d-block w-100 border" src="${images[0]}">
+                </div>
+                <div class="carousel-item mt-2">
+                    <img class="d-block w-100 border" src="${images[1]}">
+                </div>
+                <div class="carousel-item mt-2">
+                    <img class="d-block w-100 border" src="${images[2]}">
+                </div>
+                <div class="carousel-item mt-2">
+                    <img class="d-block w-100 border" src="${images[3]}">
+                </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselControls" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon bg-dark" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselControls" data-bs-slide="next">
+                <span class="carousel-control-next-icon bg-dark" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
         </div>
         `;
-        showImages(images);
+
+        divRelatedProducts.innerHTML = `
+        <br>
+        <hr class="mt-5">
+        <br>
+        <h4 class="mt-4 mb-5">Productos Relacionados</h4>
+        <div class="d-flex" id="related-images">
+        </div>
+        `;
+        // setActiveClass();
+        // showImages(images);
+        showRelated(relatedProductsArr);
     });
 
     // trae y muestra los comentarios de dicho producto
